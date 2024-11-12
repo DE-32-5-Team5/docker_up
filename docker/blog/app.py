cdeb2d327c78ad1b6a962bb4e66237ded9982fc7
Kimwonjoon <kimpass189@gmail.com>
2024-11-12 12:37:35 +0900

    streamlit - db pymysql

--- docker/blog/app.py
+++ docker/blog/app.py
@@ -2,11 +2,23 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
+import pymysql.cursors
+import os
+
+# mariadb port : localhost:3551
+def get_conn():
+    # 여기 ip나 port나 비번이나 다 바꾸기
+  conn = pymysql.connect(host=os.getenv("DB_IP","localhost"),
+                            port=int(os.getenv("MY_PORT", 3551)),
+                            user = 'fiveguys', password = 'five2024$',
+                            database = 'parkingissue',
+                            cursorclass=pymysql.cursors.DictCursor)
+  return conn
 
 def main():
     #st.set_page_config(layout="wide")
     with st.sidebar:
-        choice = option_menu("Menu", ['a', 'b', 'c'],
+        choice = option_menu("Menu", ['DB', 'PLOT'],
                             icons=['bi bi-people', 'bi bi-map'],
                             menu_icon="bi bi-app-indicator", default_index=0,
                             styles={
@@ -16,7 +28,7 @@ def main():
             "nav-link-selected": {"background-color": "#08c7b4"},
         })
 
-    if choice == "a":
+    if choice == "DB":
         st.title("Testing Page")
 
 if __name__ == '__main__':
