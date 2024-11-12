52be8993c190a2eceb4db7fcdd795f8d1ff6e923
WhiteCapella <whitecapella.official@gmail.com>
2024-11-12 11:13:36 +0900

    0.3/env complete

--- src/docker_up/setup.py
+++ src/docker_up/setup.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 import subprocess
 import os
+from docker_up.init import install
 
 def main():
   parser = argparse.ArgumentParser(description='BCS 도움말', add_help=False)
@@ -32,7 +33,8 @@ def main():
 	)
   elif args.init:
     try:
-      subprocess.run(["docker", "compose", 'up', '-d'], check=True)
+      install()		 
+      subprocess.run(['docker', 'compose', 'up', '-d'], check=True)
     except subprocess.CalledProcessError as e:
       print(f"오류 발생: {e}")
       print("docker-compose 명령어 실행에 실패했습니다. Docker Compose가 설치되어 있는지 확인해주세요.")
